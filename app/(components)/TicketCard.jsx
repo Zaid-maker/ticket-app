561fdb120d5851ed1af2f50d57ec85ffd91b9709
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-28 20:48:51 +0000

    update TicketCard

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
 import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 mr-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <Link href={`/TicketPage/${ticket}`} style={{ display: "contents" }}>
+        <h4 className="mb-1">{ticket}</h4>
+        <hr className="h-px  border-0 bg-page mb-2 "></hr>
+        <p className="whitespace-pre-wrap">{ticket}</p>
+      </Link>
     </div>
   );
 };
