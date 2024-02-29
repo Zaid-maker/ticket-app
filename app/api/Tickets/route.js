b7a345f0cf6932956fdd5b7a963b13372dc4a3ba
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-29 14:49:20 +0000

    hmm

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const tickets = await Ticket.find()
+        const tickets = await Ticket.find();
 
-        return NextResponse.json({ tickets }, { status: 200 })
+        return NextResponse.json({ tickets }, { status: 200 });
     } catch (err) {
         console.log(err);
         return NextResponse.json({ message: "Error", err }, { status: 500 });
@@ -14,10 +14,10 @@ export async function GET() {
 
 export async function POST(req) {
     try {
-        const body = await req.json()
-        const ticketData = body.formData()
+        const body = await req.json();
+        const ticketData = body.formData;
 
-        await Ticket.create(ticketData)
+        await Ticket.create(ticketData);
 
         return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
     } catch (err) {
