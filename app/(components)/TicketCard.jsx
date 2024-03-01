cf20bc573f65c91bb963556d9c35e6730dc8c724
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-01 16:24:31 +0000

    show status

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -24,7 +24,7 @@ const TicketCard = ({ ticket }) => {
             <ProgressDisplay progress={ticket.progress} />
           </div>
           <div className="ml-auto  flex items-end">
-            {/*<StatusDisplay status={ticket.status} />*/}
+            <StatusDisplay status={ticket.status} />
           </div>
         </div>
       </Link>
