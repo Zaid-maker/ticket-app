2ccf61fb0f1e3897d1e243b8a85153cd99ed0eaa
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-01 11:33:06 +0000

    test

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/TIckets", {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
       cache: "no-store",
     });
 
@@ -31,6 +31,13 @@ const Dashboard = async () => {
 
   return (
     <div className="p-5">
+      {tickets &&
+        uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex} className="mb-4">
+            <h2>{uniqueCategory}</h2>
+            <div className="lg:grid grid-cols-2 xl:grid-cols-4"></div>
+          </div>
+        ))}
       <TicketCard />
     </div>
   );
